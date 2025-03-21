87d84b1f248dca108a2004799ed3f8ec680dc149
Ignacio Fleming <ignacioflemings@gmail.com>
2025-03-21 09:30:57 -0300

    build login and register ui

--- dist/controllers/views.js
+++ dist/controllers/views.js
@@ -4,4 +4,10 @@ export default class ViewsController {
         const contacts = (await UserDAO.getContacts("67c21a97ef46abfc1c2785bd")).payload;
         res.render("home", { style: "home.css", contacts });
     };
+    static login = async (req, res, next) => {
+        res.render("login", { style: "auth.css" });
+    };
+    static register = async (req, res, next) => {
+        res.render("register", { style: "auth.css" });
+    };
 }
--- dist/routes/views.js
+++ dist/routes/views.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import ViewsController from "../controllers/views.js";
 const router = Router();
-router.get("/", ViewsController.home);
+router.get("/conversations", ViewsController.home);
+router.get("/", (req, res) => res.redirect("/conversations"));
+router.get("/login", ViewsController.login);
+router.get("/register", ViewsController.register);
 export default router;
