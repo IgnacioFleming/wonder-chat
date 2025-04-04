c8d09cf935740953ecd537314f5db4b8175fda93
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-04 16:48:17 -0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-import { server } from "./app.js";
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => console.log("Server ON"));
--- dist/utils/utils.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
-import { Types } from "mongoose";
-const __pathname = fileURLToPath(import.meta.url);
-export const __dirname = dirname(dirname(dirname(__pathname)));
-export const toObjectId = (input) => new Types.ObjectId(input);
