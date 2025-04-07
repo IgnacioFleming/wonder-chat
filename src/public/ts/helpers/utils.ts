2ece89ba536f2a96c347ed9fbc6b8b911ea75385
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-07 11:04:26 -0300

    add date to conversations list

--- src/public/ts/helpers/utils.ts
+++ src/public/ts/helpers/utils.ts
@@ -1,3 +1,19 @@
 export const getHourFromDate = (date: Date) => {
   return `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
 };
+
+export const setDateLabel = (date: Date) => {
+  const parsedDate = new Date(date);
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+  if (parsedDate.toDateString() === today.toDateString()) return "Today";
+  else if (parsedDate.toDateString() === yesterday.toDateString()) return "Yesterday";
+  else {
+    return parsedDate.toLocaleDateString("en-US", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  }
+};
