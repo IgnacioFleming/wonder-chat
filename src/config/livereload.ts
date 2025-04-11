c399e90fe667c86fe8f23541b4b83670096f0d6b
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-11 19:50:00 -0300

    finish drawing sidebar

--- src/config/livereload.ts
+++ src/config/livereload.ts
@@ -5,7 +5,7 @@ export const reloadClient = async (app: Express) => {
   if (process.env.NODE_ENV !== "development") return;
   const livereload = await import("livereload");
   const connectLivereload = await import("connect-livereload");
-
+  app.set("view cache", false);
   app.use(connectLivereload.default());
 
   const liveReloadServer = livereload.createServer();
