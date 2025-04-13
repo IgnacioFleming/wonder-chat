9769244ecee85151dc4e79b90a94ed6f053a227c
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-13 19:57:00 -0300

    build login and register ui

--- src/routes/views.ts
+++ src/routes/views.ts
@@ -3,6 +3,9 @@ import ViewsController from "../controllers/views.ts";
 
 const router = Router();
 
-router.get("/", ViewsController.home);
+router.get("/conversations", ViewsController.home);
+router.get("/", (req, res) => res.redirect("/conversations"));
+router.get("/login", ViewsController.login);
+router.get("/register", ViewsController.register);
 
 export default router;
