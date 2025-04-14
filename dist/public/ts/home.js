cca120ee6f0d8a48991c17899b4ce8c6e663e8b1
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-14 20:03:00 -0300

    add closeContactList

--- dist/public/ts/home.js
+++ dist/public/ts/home.js
@@ -5,11 +5,13 @@ if (!GlobalState.user || !GlobalState.user._id)
     window.location.href = "/login";
 const userId = GlobalState.user?._id;
 let contactId = GlobalState.selectedContact?._id;
+//key elements
 const newMessageInput = document.getElementById("newMessage");
-const allContactsSection = document.getElementById("contacts");
+export const allContactsSection = document.getElementById("contacts");
 const contactsList = document.getElementById("contacts-list");
 const newMessageBtn = document.getElementById("new-message-btn");
 const closeContactsBtn = document.getElementById("close-contacts");
+//socket events
 const socket = window.io();
 socket.emit("register", userId);
 socket.emit("getConversations", { userId });
@@ -17,18 +19,6 @@ socket.on("getConversations", ({ payload }) => {
     const conversationsContainer = document.querySelector(".conversations");
     helpers.renderListOfContacts(socket, conversationsContainer, payload);
 });
-function sendMessage(e) {
-    if (!userId)
-        return;
-    if (e.key !== "Enter")
-        return;
-    e.preventDefault();
-    const content = newMessageInput.value.trim();
-    if (!content)
-        return;
-    helpers.sendMessage(socket, { author: userId.toString(), receiver: contactId?.toString() || "", content });
-    newMessageInput.value = "";
-}
 socket.on("sendMessage", (message) => {
     if (!userId)
         return;
@@ -36,21 +26,15 @@ socket.on("sendMessage", (message) => {
     helpers.renderSingleMessage(message, userId.toString(), messagesSection);
     messagesSection.scrollTop = messagesSection.scrollHeight;
 });
-newMessageInput.addEventListener("keydown", sendMessage);
+//event listeners
+newMessageInput.addEventListener("keydown", (e) => helpers.sendMessage(e, socket, newMessageInput));
 newMessageBtn.addEventListener("click", async () => {
     if (!userId)
         return;
     const contacts = await getContacts(userId);
-    console.log(contacts);
     allContactsSection.classList.replace("hidden", "block");
     if (!contacts || contacts?.length <= 0)
         return;
     helpers.renderListOfContacts(socket, contactsList, contacts);
 });
-closeContactsBtn.addEventListener("click", () => {
-    allContactsSection.classList.add("close");
-    setTimeout(() => {
-        allContactsSection.classList.replace("block", "hidden");
-        allContactsSection.classList.remove("close");
-    }, 290);
-});
+closeContactsBtn.addEventListener("click", helpers.closeContactsList);
--- src/public/ts/home.ts
+++ src/public/ts/home.ts
@@ -12,11 +12,15 @@ declare global {
 if (!GlobalState.user || !GlobalState.user._id) window.location.href = "/login";
 const userId = GlobalState.user?._id;
 let contactId = GlobalState.selectedContact?._id;
+
+//key elements
 const newMessageInput = document.getElementById("newMessage") as HTMLTextAreaElement;
-const allContactsSection = document.getElementById("contacts") as HTMLElement;
+export const allContactsSection = document.getElementById("contacts") as HTMLElement;
 const contactsList = document.getElementById("contacts-list") as HTMLDivElement;
 const newMessageBtn = document.getElementById("new-message-btn") as HTMLElement;
 const closeContactsBtn = document.getElementById("close-contacts") as HTMLElement;
+
+//socket events
 
 const socket = window.io();
 
@@ -27,16 +31,6 @@ socket.on("getConversations", ({ payload }: { payload: PopulatedConversation[] }
   helpers.renderListOfContacts(socket, conversationsContainer, payload);
 });
 
-function sendMessage(e: KeyboardEvent) {
-  if (!userId) return;
-  if (e.key !== "Enter") return;
-  e.preventDefault();
-  const content = newMessageInput.value.trim();
-  if (!content) return;
-  helpers.sendMessage(socket, { author: userId.toString(), receiver: contactId?.toString() || "", content });
-  newMessageInput.value = "";
-}
-
 socket.on("sendMessage", (message: ClientMessage) => {
   if (!userId) return;
   const messagesSection = document.querySelector("section.messages") as HTMLElement;
@@ -44,20 +38,15 @@ socket.on("sendMessage", (message: ClientMessage) => {
   messagesSection.scrollTop = messagesSection.scrollHeight;
 });
 
-newMessageInput.addEventListener("keydown", sendMessage);
+//event listeners
+
+newMessageInput.addEventListener("keydown", (e) => helpers.sendMessage(e, socket, newMessageInput));
 newMessageBtn.addEventListener("click", async () => {
   if (!userId) return;
   const contacts = await getContacts(userId);
-  console.log(contacts);
   allContactsSection.classList.replace("hidden", "block");
   if (!contacts || contacts?.length <= 0) return;
   helpers.renderListOfContacts(socket, contactsList, contacts);
 });
 
-closeContactsBtn.addEventListener("click", () => {
-  allContactsSection.classList.add("close");
-  setTimeout(() => {
-    allContactsSection.classList.replace("block", "hidden");
-    allContactsSection.classList.remove("close");
-  }, 290);
-});
+closeContactsBtn.addEventListener("click", helpers.closeContactsList);
