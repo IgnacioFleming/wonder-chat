e51a2aa1b59033b96ad8ed1389434701f0a83c23
Ignacio Fleming <ignacioflemings@gmail.com>
2025-04-23 20:38:00 -0300

    add production database

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  mongo_url_dev: process.env.MONGO_URL_DEV as string,
+  mongo_url_dev: process.env.NODE_ENV === "production" ? (process.env.MONGO_URL_PROD as string) : (process.env.MONGO_URL_DEV as string),
   secret: process.env.SECRET as string,
 };
